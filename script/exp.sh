463a9e96a9d38ee92f27ac5013c2171c9b1cabfa
stgkrt <taro.stst@gmail.com>
2024-07-24 22:56:55 +0900

    :bookmark: basic

--- script/exp.sh
+++ script/exp.sh
@@ -5,7 +5,10 @@ if [ -d /kaggle/working/$exp_name ]; then
   suffix=$(date "+_%Y%m%d_%H%M%S")
   exp_name=$exp_name$suffix
 fi
+
+# 作成したexp_nameを表示
 echo "exp_name: $exp_name"
+
 # train
 python src/train.py exp_name="$exp_name" splits=fold0
 python src/train.py exp_name="$exp_name" splits=fold1
